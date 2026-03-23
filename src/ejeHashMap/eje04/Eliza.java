fc66f2deec71b68035ce5d60f7b55471881a0b22
Alumno1 <dazaran8@gmail.com>
2026-03-23 13:31:19 +0100

    Version primera

--- src/ejeHashMap/eje04/Eliza.java
+++ src/ejeHashMap/eje04/Eliza.java
@@ -1,9 +1,40 @@
 package ejeHashMap.eje04;
 
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class Eliza {
-
-	public Eliza() {
-		// TODO Auto-generated constructor stub
+	
+	public static void main(String[] args) {
+		
+		var map = new HashMap <String,String> ();
+		
+		map.put("HOLA", "Hola, ¿que tal?");
+		map.put("ENCANTADO", "Encantado de conocerte yo tambien");
+		map.put("ADIOS", "Adios espero volverte a ver pronto");
+		map.put("HORA" , "Lo siento, no llevo reloj");
+		map.put("NOMBRE", "Mi nombre es Eliza");
+		
+		String msgDefault ="Lo siento, no te comprendo";
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Bienvenido al sistema Eliza 0.0");
+		String frase;
+		boolean fin =false;
+		do {
+			frase = sc.nextLine();
+			boolean encontrada = false;
+			for (String clave:map.keySet()) {
+				if(frase.toUpperCase().contains(clave)) {
+					encontrada = true;
+					System.out.println(">"+map.get(clave));
+					if (frase.toUpperCase().contains("ADIOS")) {
+						fin=true;
+					}
+					break;
+				}
+			}
+		} while (!fin);
+		System.out.println("Muchas gracias por conectarse");
 	}
-
 }
